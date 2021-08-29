ded64672b893f820d3b8d3cb46bbaf593397f89a
איתיאל מועד <ItielM@kkl.org.il>
2021-08-29 22:25:31 +0300

    first commit

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute , Router} from '@angular/router';
+import {
+  Validators,
+  FormBuilder,
+  FormGroup,
+  FormControl,
+} from "@angular/forms";
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  projectForm:FormGroup;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.projectForm= new FormGroup({
+      projectId: new FormControl('',[Validators.required]),
+      projectName: new FormControl('',[Validators.required])
+      
+   });
+  }
+
+}
